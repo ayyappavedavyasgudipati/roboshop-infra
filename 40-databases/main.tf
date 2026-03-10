09abbf8ca9020b066ec8fb4c1023505906dac874
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-03-11 00:30:15 +0530

    terraform-ansible

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -6,7 +6,31 @@ resource "aws_instance" "mongodb" {
   tags = merge(
     local.ec2_tags,
     {
-      Name = "${var.project}-${var.environment}-bastion"
+      Name = "${var.project}-${var.environment}-mongodb"
     }
   )
+}
+
+resource "terraform_data" "bootstrap" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mongodb.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" 
+    destination = "/tmp/bootstrap.sh"    
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh"]
+  }
 }
