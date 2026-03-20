dc0622e53f3fa913c43836ca0777897945a337db
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-03-20 23:29:10 +0530

    sg rules, alb and private

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -290,6 +290,6 @@ resource "aws_security_group_rule" "frontend_alb_public" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  source_security_group_id = "[0.0.0.0/0]"
+  cidr_blocks = "[0.0.0.0/0]"
   security_group_id = local.frontend_alb_sg_id
 }
