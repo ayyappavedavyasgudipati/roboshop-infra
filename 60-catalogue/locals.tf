b6ab8a9ee3fd5f303650fb718c9853e436804fab
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-03-20 23:40:57 +0530

    sg rules, alb and private

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,5 +2,5 @@ locals {
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_ids = split(",",data.aws_ssm_parameter.private_subnet_ids.value)[0]
     vpc_id = data.aws_ssm_parameter.vpc_id.value
-    backende_alb_listner_arn = data.aws_ssm_parameter.backende_alb_listner_arn.value
+    backend_alb_listner_arn = data.aws_ssm_parameter.backend_alb_listner_arn.value
 }
