559b12478518536c0dc92b5dd1e9aef32c29d141
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-03-22 23:56:42 +0530

    sg rules, alb and private

--- 80-frontend-alb/parameter.tf
+++ 80-frontend-alb/parameter.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "backende_alb_listner_arn" {
-    name = "/${var.project}/${var.environment}/backende_alb_listner_arn"
+resource "aws_ssm_parameter" "frontend_alb_listner_arn" {
+    name = "/${var.project}/${var.environment}/frontend_alb_listner_arn"
     type  = "String"
-    value = aws_lb_listener.http.arn
+    value = aws_lb_listener.https.arn
 }
