611e4156baf888a3d154e313b97d7e97e87c15cd
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-04-05 12:26:24 +0530

    ansible roboshop roles

--- 90-roboshop-components/main.tf
+++ 90-roboshop-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = source = "git::https://github.com/ayyappavedavyasgudipati/terraform-roboshop-components.git"
+    source = "git::https://github.com/ayyappavedavyasgudipati/terraform-roboshop-components.git"
     component    = each.key
     rule_priority = each.value.rule_priority
 }
